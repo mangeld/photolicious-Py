bd3850ea2379e122f0db1095cfec2900f7dd6731
Miguel Ángel Durán González <conderser@gmail.com>
2015-01-20 22:58:53 +0100

    finished method for squared blurred photos

--- photoTests.py
+++ photoTests.py
@@ -1,22 +1,30 @@
 from __future__ import print_function
 from PIL import Image, ImageFilter, ImageEnhance
 
-im = Image.open('JPEG/Fraag 4_def_v.2.jpg')
+im = Image.open('JPEG/porsche.jpg')
 #print(im.format, im.size, im.mode)
 
-width = im.size[0]
-height = im.size[1]
+def makeBlurredSquare( im, destination, blur=25, quality=95, blurDarkness=0.86 ):
+	width = im.size[0]
+	height = im.size[1]
 
-if width < height:
-	newWidth   = height
-	newHeight  = ( newWidth * newWidth ) / width
-	blurredImg = im.resize( (newWidth, newHeight), Image.BICUBIC )
-	blurredImg = blurredImg.filter( ImageFilter.GaussianBlur( 25 ) )
-	blurredImg = blurredImg.crop( (0, (newHeight - height) / 2 , height, height + ((newHeight - height)/2)) )
-	enhancer = ImageEnhance.Brightness( blurredImg )
-	enhancer = enhancer.enhance(0.86)
-	enhancer.paste( im, ( (newWidth - width) / 2 , 0) )
-	enhancer.save( 'test.jpg', quality=95 )
-
-
-
+	if width < height:
+		newWidth   = height
+		newHeight  = ( newWidth * newWidth ) / width
+		#Resize so the short side == the initial long side & apply blur
+		blurredImg = im.resize( (newWidth, newHeight), Image.BICUBIC ).filter( ImageFilter.GaussianBlur( blur ) )
+		#Crop the background to be 1:1
+		blurredImg = blurredImg.crop( (0, (newHeight - height) / 2 , height, height + ((newHeight - height)/2)) )
+		#Make it darker so it doesn't draw attention
+		blurredImg = ImageEnhance.Brightness( blurredImg ).enhance( blurDarkness )
+		#Paste the original image in the center
+		blurredImg.paste( im, ( (newWidth - width)/2 , 0) )
+		blurredImg.save( destination, quality=quality )
+	else:
+		newWidth   = ( width * width ) / height
+		newHeight  = width
+		blurredImg = im.resize( ( newWidth, newHeight ), Image.BICUBIC ).filter( ImageFilter.GaussianBlur( blur ) )
+		blurredImg = blurredImg.crop( ( (newWidth - newHeight)/2 , 0, newWidth - ((newWidth - newHeight)/2) ,newHeight ) )
+		blurredImg = ImageEnhance.Brightness( blurredImg ).enhance( blurDarkness )
+		blurredImg.paste( im, ( 0, (newHeight-height)/2) )
+		blurredImg.save( destination, quality=quality )
